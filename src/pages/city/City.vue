b3c0d043ffa810543203f5c4f9348204b6d57b04
j-p <2061351540@qq.com>
2022-08-24 16:54:06 +0800

    city ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,8 +3,8 @@
   <div>
    <Header></Header>
    <Search></Search>
-   <List></List>
-   <Alphabet></Alphabet>
+   <List :cities='cities' :hotCities='hotCities'></List>
+   <Alphabet :cities='cities'></Alphabet>
   </div>
 </template>
 
@@ -13,10 +13,13 @@ import Alphabet from './components/Alphabet.vue'
 import Header from './components/Header.vue'
 import List from './components/List.vue'
 import Search from './components/Search.vue'
+import axios from 'axios'
 export default {
 name:'City',
   data () {
     return {
+      cities:{},
+      hotCities:[]
     }
   },
   components:{
@@ -24,6 +27,23 @@ name:'City',
     Search,
     List,
     Alphabet
+  },
+  mounted(){
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo(){
+      axios.get('/api/city.json').then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc(res){
+      // console.log(res);
+      res=res.data
+      if(res.ret && res.data){
+        const data=res.data;
+        this.cities=data.cities
+        this.hotCities=data.hotCities
+      }
+    }
   },
 }
 </script>
